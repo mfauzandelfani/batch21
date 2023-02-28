56585fa999cf865c304fc949449b49f94f704579
mfauzandelfani <fauzan18ti@mahasiswa.pcr.ac.id>
2023-02-28 11:40:52 +0700

    first commit

---
+++ day01/logicaloperator.js
@@ -0,0 +1,14 @@
+let a = true && true;
+console.log(a);
+
+const b = true && false;
+console.log(b);
+
+const c = 10 === 10 && 10 !== 1;
+console.log(c);
+
+const d = true || false;
+console.log(d);
+
+const e = 15>12
+console.log(e);
---
+++ day02/destructure.js
@@ -0,0 +1,12 @@
+const program = ['java','python','js','sql','oracle','postgres']
+const [java,python,js,...sql] = program
+
+console.log();
+
+const object = {
+    fistname : 'John',
+    Lastname : 'Doe',
+    Age : 17
+}
+const{fistname,Lastname,Age} = object
+console.log();
